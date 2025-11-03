d8e96ef0292c2c42c24da596657b7b1dc2a2d0b5
hangsharajboro <hangsharajboro@gmail.com>
2025-11-03 07:42:38 +0530

    Add files via upload

---
+++ YoutubeDownloader/YoutubeDownloader/BLL/AudioBusinessLogicLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloader.BLL
+{
+    internal class AudioBusinessLogicLayer
+    {
+        public string Bitrate { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ YoutubeDownloader/YoutubeDownloader/BLL/BusinessLogicLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader.BLL
+{
+    internal class BusinessLogicLayer
+    {
+        public string url { get; set; }
+        public string label { get; set; }
+        public string quality { get; set; }
+        public string filename { get; set; }
+    }
+}
